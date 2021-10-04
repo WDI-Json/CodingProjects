a9ad110e76ed0c8ec2e53e8bb4e07a73544c50c6
Wouter Dijks <wutu@Wouters-MacBook-Pro.local>
2021-10-04 21:05:14 +0200

    Aanmaken database connection class die later voor de restapi gebruikt kan worden

---
+++ WWI/RESTAPI/cls_database.php
@@ -0,0 +1,30 @@
+<?php
+/*Deze class is bedoeld om verbinding te maken met de database. Dit is onderdeel van de REST-api.
+ */
+class database {
+    //parameters
+    private $host = 'localhost';
+    private $db_name = 'Mysql';
+    private $username = 'root';
+    private $password = '123456';
+    private $connection;
+
+    //verbinding met DB
+    public function connect() {
+        $this -> connection = null;
+    /*Tijdens mijn poging om te verbinden wil ik het weten als er iets misgaat.
+    Daarom vang ik een eventuele fout af met een Catch.
+    */
+        try {
+            //connectionstring
+            $this->connection = new PDO('mysql:host=' . $this->host . ';dbname=' .
+            $this->db_name, $this->username, $this->password);
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $errordescription) {
+            echo 'Connection error: ' . $errordescription->getMessage();
+        }
+        return $this->connection;
+    }
+
+}
+?>
