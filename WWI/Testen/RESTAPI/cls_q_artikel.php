8d5c59dbeac2b5b7cbf472e3870def9a303c25f4
wutu <wouterdijks@hotmail.com>
2021-11-13 16:25:36 +0100

    restapi adding

--- WWI/Testen/RESTAPI/cls_q_artikel.php
+++ WWI/Testen/RESTAPI/cls_q_artikel.php
@@ -1,6 +1,13 @@
 <?php
 
-class cls_q_artikel
+class cls_q_artikel extends database
 {
-
+    public function getArticle(){
+        $sql = 'SELECT * FROM StockItems';
+        $statement = $this->connect()->query($sql);
+        while($row = $statement->fetch()){
+            echo 'de naam van het artikel is ' .  $row['stockitemname'] .
+            ' en zal ' . $row['RecommendedRetailPrice'] . ' kosten. ' . '<br>';
+        }
+    }
 }
--- WWI/Testen/test.php
+++ WWI/Testen/test.php
@@ -1,1 +1,8 @@
+
+
 <?php
+include_once 'RESTAPI/cls_q_artikel.php';
+$test = new cls_q_artikel;
+$test->getArticle();
+
+?>
