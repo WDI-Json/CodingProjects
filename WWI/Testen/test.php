bbb164f59caaf5f94ec866b5ca8c5a40a441fcbe
wutu <wouterdijks@hotmail.com>
2021-11-19 14:41:40 +0100

    conceptje werkend

--- WWI/Testen/test.php
+++ WWI/Testen/test.php
@@ -6,14 +6,6 @@ function Opvullen($tekst, $lengte)
         str_pad($tekst, $lengte);
 }
 
-/*
- Maak nu een functie PrintResultaat($result, $titel), waaraan je een array die je vanuit de Select()
-functie krijgt, kan meegeven en tevens een titel die netjes boven het resultaat wordt getoond.
-Elke rij uit het resultaat wordt netjes op een volgende regel geprint.
-Je kunt de kopjes die je boven de informatie zou willen hebben ophalen uit
-het array met de functie array_keys().
-Wanneer je het volgende voorbeeld gebruikt:
-  */
 function PrintResultaat($query, $titel)
 {
     $con = new database();
@@ -25,19 +17,51 @@ function PrintResultaat($query, $titel)
     $stmt->execute();
     $result = $stmt->fetchall(PDO::FETCH_ASSOC);
     $column = array_keys($result[0]);
-    print $column[0] . "\t" . "|" . "\t" . $column[1] . "|" . "\t" . $column[2] . "\t" . "|" . "\t" . $column[3] . "\n";
-    foreach ($result as $row) {
-        print $row['store_id'] . "\t" . "\t" . "\t" . "|" . "\t";
-        print $row['manager_staff_id'] . "\t" . "\t" . "\t" . "\t" . "|" . "\t";
-        print $row['address_id'] . "\t" . "\t" . "\t" . "|" . "\t";
-        print $row['last_update'] . "\n";
-    }
+    foreach($column as $head)
+    {
+        print($head) . "\t" ."|" ."\n";
+        foreach ($result as $row) {
+            print($row[$head] . "\t" . "\t" . "\t"  );
+            /*
+            print $row[$column[0]] . "\t" . "\t" . "\t" . "|" . "\t";
+            print $row[$column[1]] . "\t" . "\t" . "\t" . "\t" . "|" . "\t";
+            print $row[$column[2]] . "\t" . "\t" . "\t" . "|" . "\t";
+            print $row[$column[3]] . "\n";*/
+        }}
 
 
 }
 
 
-PrintResultaat("SELECT * FROM store", "Overzicht van de winkels:");
+PrintResultaat("SELECT store_id, manager_staff_id, address_id, last_update FROM store", "Overzicht van de winkels:");
+/*PrintResultaat("SELECT
+film_info.title AS Filmtitel,
+film_info.release_year AS Omschrijving, 
+film_info.rating AS Leeftijd,
+SUM(betaling.amount) AS Bedrag 
+FROM payment AS Betaling
+LEFT JOIN rental AS huur_info
+ON huur_info.rental_id = betaling.rental_id
+LEFT JOIN inventory AS inventory
+ON inventory.inventory_id = huur_info.inventory_id
+LEFT JOIN film AS film_info
+ON film_info.film_id = inventory.film_id
+WHERE YEAR(payment_date) = '2015'
+GROUP BY 
+film_info.title,
+film_info.release_year,
+film_info.rating
+LIMIT 4", "Opbrengst per film in 2015");
+
+PrintResultaat("SELECT 
+act.first_name AS Voornaam, 
+act.last_name AS Achternaam,
+act.date_of_birth AS Geboortedatum,
+actor_id  
+FROM actor as act
+ORDER BY date_of_birth ASC
+LIMIT 1", "Oudste acteur");
+*/
 
 
 ?>
