b1476e80c0cb427d18af07cc56a679acd5f4a53e
wutu <wouterdijks@hotmail.com>
2021-11-19 11:08:21 +0100

    het werkt eindelijk!

--- WWI/Testen/test.php
+++ WWI/Testen/test.php
@@ -1,11 +1,11 @@
-
-
 <?php
 require_once('RESTAPI/cls_database.php');
-function Opvullen($tekst, $lengte) {
+function Opvullen($tekst, $lengte)
+{
     return
         str_pad($tekst, $lengte);
 }
+
 /*
  Maak nu een functie PrintResultaat($result, $titel), waaraan je een array die je vanuit de Select()
 functie krijgt, kan meegeven en tevens een titel die netjes boven het resultaat wordt getoond.
@@ -19,28 +19,25 @@ function PrintResultaat($query, $titel)
     $con = new database();
     $dbc = $con->connect();
     print $titel . "\n";
-    $column = $dbc->query("DESCRIBE store");
-    print $column;
 
-    foreach($dbc->query($query) as $row) {
-        print $row[0] . "\t" . "\t" ."|" . "\t" ;
-        print $row[1] . "\t" . "\t" . "\t" ."|" ."\t";
-        print $row[2] . "\t" . "\t" . "\t" ."|";
-        print $row[3] . "\n";
+    $sql = $query;
+    $stmt = $dbc->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->fetchall(PDO::FETCH_ASSOC);
+    $column = array_keys($result[0]);
+    print $column[0] . "\t" . "|" . "\t" . $column[1] . "|" . "\t" . $column[2] . "\t" . "|" . "\t" . $column[3] . "\n";
+    foreach ($result as $row) {
+        print $row['store_id'] . "\t" . "\t" . "\t" . "|" . "\t";
+        print $row['manager_staff_id'] . "\t" . "\t" . "\t" . "\t" . "|" . "\t";
+        print $row['address_id'] . "\t" . "\t" . "\t" . "|" . "\t";
+        print $row['last_update'] . "\n";
     }
 
-    }
+
+}
 
 
 PrintResultaat("SELECT * FROM store", "Overzicht van de winkels:");
-
-   /* $sth = null;
-    $dbh = null;*/
-
-
-
-
-
 
 
 ?>
