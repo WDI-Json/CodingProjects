4790da3841807bb197d27c5cdd73d78daa48d2dd
WDI_JSON <wouterdijks@hotmail.com>
2022-02-02 20:17:47 +0100

    mass commit voor de tussentijdse files. hij is stuk nu

--- webdev/flix/app/controllers/reviews_controller.rb
+++ webdev/flix/app/controllers/reviews_controller.rb
@@ -1,7 +1,29 @@
 class ReviewsController < ApplicationController
-
+    
+    
     def index
         @movie = Movie.find(params[:movie_id])
         @reviews = @movie.reviews
     end
+
+    def new
+        @movie = Movie.find(params[:movie_id])
+        @reviews = @movie.reviews.new
+    end
+
+    def create
+        @movie = Movie.find(params[:movie_id])
+        @reviews = @movie.reviews.new(params[:registration])
+        if @reviews.save 
+            redirect_to(@movie)
+        else
+            render :new
+        end
+    end
+
+    private 
+    def set_movie
+        @movie = Movie.find(params[:movie_id])
+    end
+
 end
