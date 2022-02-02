08e537943365fbff4162876892ba8a2fd36f7478
WDI_JSON <wouterdijks@hotmail.com>
2022-02-02 21:11:25 +0100

    chapter 24

--- webdev/flix/app/controllers/reviews_controller.rb
+++ webdev/flix/app/controllers/reviews_controller.rb
@@ -1,29 +1,31 @@
 class ReviewsController < ApplicationController
-    
+    before_action :set_movie
     
     def index
-        @movie = Movie.find(params[:movie_id])
         @reviews = @movie.reviews
     end
 
     def new
-        @movie = Movie.find(params[:movie_id])
-        @reviews = @movie.reviews.new
+        @review = @movie.reviews.new
     end
 
     def create
-        @movie = Movie.find(params[:movie_id])
-        @reviews = @movie.reviews.new(params[:registration])
-        if @reviews.save 
-            redirect_to(@movie)
-        else
+        @review = @movie.reviews.new(review_params)
+        if @review.save
+            redirect_to movie_reviews_path(@movie),
+                          notice: "Thanks for your review!"
+          else
             render :new
+          end
         end
-    end
 
-    private 
+    private
+
+    def review_params
+      params.require(:review).permit(:name, :comment, :stars)
+    end
+  
     def set_movie
-        @movie = Movie.find(params[:movie_id])
+      @movie = Movie.find(params[:movie_id])
     end
-
 end
--- webdev/flix/app/models/review.rb
+++ webdev/flix/app/models/review.rb
@@ -3,7 +3,7 @@ class Review < ApplicationRecord
 
   validates :name, presence: true
 
-  validates :comments, length: { minimum: 4 }
+  validates :comment, length: { minimum: 4 }
 
   STARS = [1, 2, 3, 4, 5]
   validates :stars, inclusion: {
