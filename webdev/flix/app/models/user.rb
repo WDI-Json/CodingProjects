5714c44571d82b30553f97fed4f4fc0c802687e0
WDI_JSON <wouterdijks@hotmail.com>
2022-02-05 21:13:07 +0100

    backend voor inloggen

--- webdev/flix/app/models/user.rb
+++ webdev/flix/app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :name, presence: true
+  validates :email, email: {mode: :strict, require_fqdn: true},
+    uniqueness: { case_sensitive: false} 
+
+  validates :password, length: { minimum: 10, allow_blank: true }
 end
