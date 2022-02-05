fd1978ccae0b0d1cc0f68d92223a8a83f0d0e2c7
WDI_JSON <wouterdijks@hotmail.com>
2022-02-05 22:15:03 +0100

    user can sign up

--- webdev/flix/app/controllers/users_controller.rb
+++ webdev/flix/app/controllers/users_controller.rb
@@ -13,7 +13,19 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = User.create(params[:id])
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to @user, notice: "Thanks signing up!"
+        else
+          render :new
+        end
+      end
+
+    private
+
+    def user_params
+        params.require(:user).
+        permit(:name, :email, :password, :password_confirmation)
     end
 
 end
