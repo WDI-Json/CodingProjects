8b30ba28a7904a10665903c5fd2342c5b26879c0
WDI_JSON <wouterdijks@hotmail.com>
2022-02-06 11:31:03 +0100

    werkende authflow. nu met username toevoegen

--- webdev/flix/app/controllers/sessions_controller.rb
+++ webdev/flix/app/controllers/sessions_controller.rb
@@ -4,6 +4,14 @@ class SessionsController < ApplicationController
     end
 
     def create
+        @user = User.find_by(:email params[:email])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect_to user, notice: "Welcome back, #{user.name}!"
+        else 
+            flash.now[:alert] = "Invalid Email/Password combination. Try again!"
+            render :new
+        end
     end
 
     def destroy
--- webdev/flix/config/routes.rb
+++ webdev/flix/config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   get "signup" => "users#new"
   
   resource :session, only: [:new, :create, :destroy]
+  get "signin" => "sessions#new"
 end
