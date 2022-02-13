1be885fc650bebb2580dad1495c5d19d68c6c15c
WDI_JSON <wouterdijks@hotmail.com>
2022-02-13 19:50:14 +0100

    admin autorisatie

--- webdev/flix/app/controllers/application_controller.rb
+++ webdev/flix/app/controllers/application_controller.rb
@@ -21,4 +21,14 @@ class ApplicationController < ActionController::Base
     helper_method :current_user?
   
     helper_method :current_user
+
+    def require_admin
+      unless current_user_admin?
+        redirect_to root_url, alert: "Unauthorized access!"
+      end
+    end
+    helper_method :current_user_admin?
+    def current_user_admin?
+      current_user && current_user.admin?
+    end
   end
