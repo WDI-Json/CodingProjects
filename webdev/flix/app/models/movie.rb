4a7a93ae6b1bad0694f846e9595d519fcc8846f1
WDI_JSON <wouterdijks@hotmail.com>
2022-02-22 13:53:14 +0100

    scopes moviemodel

--- webdev/flix/app/models/movie.rb
+++ webdev/flix/app/models/movie.rb
@@ -21,18 +21,12 @@ class Movie < ApplicationRecord
 
     validates :rating, inclusion: {in: RATINGS}
     
+    scope :released, -> { where("released_on < ?", Time.now).order("released_on desc") }
+    scope :upcoming, -> { where("released_on > ?", Time.now).order("released_on asc") }  
+    scope :recent, ->(max=5) { released.limit(max) }
+    scope :hits, -> {released.where("total_gross >= 300000000").order(total_gross: :desc)}
+    scope :flops, -> {released.where("total_gross < 22500000").order(total_gross: :asc)}
 
-    def self.released
-        where("released_on < ?", Time.now).order(released_on: :desc) 
-    end
-
-    def self.hits
-        where("total_gross >= 300000000").order(total_gross: :desc)
-    end
-
-    def self.flops
-        where("total_gross < 22500000").order(total_gross: :asc)
-    end
 
     def self.recently_added
         order("created_at desc").limit(3)
