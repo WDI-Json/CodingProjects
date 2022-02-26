7e1f224e196cb6af00bdb5099670ce2ae5269c5c
WDI_JSON <wouterdijks@hotmail.com>
2022-02-26 10:41:48 +0100

    storage fix Co-authored-by: Redmar Kerkhoff <redmar@users.noreply.github.com>

--- webdev/flix/db/seeds.rb
+++ webdev/flix/db/seeds.rb
@@ -21,8 +21,7 @@ movies = [
     rating: "PG-13",
     total_gross: 1_223_641_414,
     director: "Anthony Russo",
-    duration: "181 min",
-    image_file_name: "avengers-end-game.png"
+    duration: "181 min"
   },
   {
     title: "Captain Marvel",
@@ -34,8 +33,7 @@ movies = [
     rating: "PG-13",
     total_gross: 1_110_662_849,
     director: "Anna Boden",
-    duration: "124 min",
-    image_file_name: "captain-marvel.png"
+    duration: "124 min"
   },
   {
     title: "Black Panther",
@@ -47,8 +45,7 @@ movies = [
     rating: "PG-13",
     total_gross: 1_346_913_161,
     director: "Ryan Coogler",
-    duration: "134 min",
-    image_file_name: "black-panther.png"
+    duration: "134 min"
   },
   {
     title: "Avengers: Infinity War",
@@ -60,8 +57,7 @@ movies = [
     rating: "PG-13",
     total_gross: 2_048_359_754,
     director: "Anthony Russo",
-    duration: "149 min",
-    image_file_name: "avengers-infinity-war.png"
+    duration: "149 min"
   },
   {
     title: "Green Lantern",
@@ -73,8 +69,7 @@ movies = [
     rating: "PG-13",
     total_gross: 219_851_172,
     director: "Martin Campbell",
-    duration: "114 min",
-    image_file_name: "green-lantern.png"
+    duration: "114 min"
   },
   {
     title: "Fantastic Four",
@@ -86,8 +81,7 @@ movies = [
     rating: "PG-13",
     total_gross: 168_257_860,
     director: "Josh Trank",
-    duration: "100 min",
-    image_file_name: "fantastic-four.png"
+    duration: "100 min"
   },
   {
     title: "Iron Man",
@@ -101,8 +95,7 @@ movies = [
     rating: "PG-13",
     total_gross: 585_366_247,
     director: "Jon Favreau",
-    duration: "126 min",
-    image_file_name: "ironman.png"
+    duration: "126 min"
   },
   {
     title: "Superman",
@@ -116,8 +109,7 @@ movies = [
     rating: "PG",
     total_gross: 300_451_603,
     director: "Richard Donner",
-    duration: "143 min",
-    image_file_name: "superman.png"
+    duration: "143 min"
   },
   {
     title: "Spider-Man",
@@ -132,8 +124,7 @@ movies = [
     rating: "PG-13",
     total_gross: 825_025_036,
     director: "Sam Raimi",
-    duration: "121 min",
-    image_file_name: "spiderman.png"
+    duration: "121 min"
   },
   {
     title: "Batman",
@@ -146,8 +137,7 @@ movies = [
     rating: "PG-13",
     total_gross: 411_348_924,
     director: "Tim Burton",
-    duration: "126 min",
-    image_file_name: "batman.png"
+    duration: "126 min"
   },
   {
     title: "Catwoman",
@@ -159,8 +149,7 @@ movies = [
     rating: "PG-13",
     total_gross: 82_102_379,
     director: "Jean-Christophe 'Pitof' Comar",
-    duration: "101 min",
-    image_file_name: "catwoman.png"
+    duration: "101 min"
   },
   {
     title: "Wonder Woman",
@@ -172,8 +161,7 @@ movies = [
     rating: "PG-13",
     total_gross: 821_847_012,
     director: "Patty Jenkins",
-    duration: "141 min",
-    image_file_name: "wonder-woman.png"
+    duration: "141 min"
   }
 ]
 
