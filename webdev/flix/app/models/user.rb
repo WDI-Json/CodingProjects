82f093c18b66d7f1ee2e744a47571dcba2c9857d
WDI_JSON <wouterdijks@hotmail.com>
2022-02-26 11:19:36 +0100

    profile_pictures for users!

--- webdev/flix/app/models/user.rb
+++ webdev/flix/app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
-  before_save :format_username
   has_many :reviews, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :favorite_movies, through: :favorites, source: :movie
   
+  has_one_attached :profile_image
+  validate :acceptable_profile_image
+
   scope :by_name, -> { order(:name) }
   scope :not_admins, -> { by_name.where(admin: false) }
 
@@ -21,8 +23,16 @@ class User < ApplicationRecord
     Digest::MD5::hexdigest(email.downcase)
   end
 
-  def format_username
-    self.username = username.downcase
+  def acceptable_profile_image
+    return unless profile_image.attached?
+  
+    unless profile_image.blob.byte_size <= 1.megabyte
+      errors.add(:profile_image, "is too big")
+    end
+  
+    acceptable_types = ["image/jpeg", "image/png"]
+    unless acceptable_types.include?(profile_image.content_type)
+      errors.add(:profile_image, "must be a JPEG or PNG")
+    end
   end
-
 end
