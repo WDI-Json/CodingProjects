c88cf69b2244b6a6b6664c979835831b4d967392
WDI_JSON <wouterdijks@hotmail.com>
2022-02-26 08:29:41 +0100

    oeps. ff aparte branch aanmaken.

--- webdev/flix/app/helpers/movies_helper.rb
+++ webdev/flix/app/helpers/movies_helper.rb
@@ -1,29 +1,37 @@
 module MoviesHelper
-    def total_gross(movie)
-        if movie.flop?
-            "Flop!"
-          else
-            number_to_currency(movie.total_gross, precision: 0)
-          end
-    end
+  def total_gross(movie)
+      if movie.flop?
+          "Flop!"
+        else
+          number_to_currency(movie.total_gross, precision: 0)
+        end
+  end
 
-    def year_of(movie)
-        movie.released_on.strftime("%Y")
-    end
+  def year_of(movie)
+      movie.released_on.strftime("%Y")
+  end
 
-    def average_stars(movie)
-      if movie.average_stars.zero?
-        content_tag(:strong, "No reviews")
-      else
-        "*" * movie.average_stars.round
-      end
+  def average_stars(movie)
+    if movie.average_stars.zero?
+      content_tag(:strong, "No reviews")
+    else
+      "*" * movie.average_stars.round
     end
+  end
 
-    def nav_link_to(text, url)
-      if current_page?(url)
-        link_to(text, url, class: "active")
-      else
-        link_to(text, url)
-      end
+  def nav_link_to(text, url)
+    if current_page?(url)
+      link_to(text, url, class: "active")
+    else
+      link_to(text, url)
     end
+  end
+
+  def main_image(movie)
+    if movie.main_image.attached?
+      image_tag movie.main_image
+    else
+      image_tag "placeholder.png"
+    end
+  end
 end
