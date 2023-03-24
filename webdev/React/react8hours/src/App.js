cc4e22a522afe9ddb66a132c3ef02700ca15d72c
WDijks <w.dijks@arcady.nl>
2023-03-24 10:37:56 +0100

    module 2

--- webdev/React/react8hours/src/App.js
+++ webdev/React/react8hours/src/App.js
@@ -1,7 +1,31 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return <div className="App">Hello world!</div>;
+
+  return <div className="App">
+    <User name="Wouter" age={29} email="wouterdijks.com" />
+    <Job salary={239000} position="React developer" company="Arcady"/>
+  </div>;
 }
+
+const User = (props) => {
+  return (
+    <div>
+     <h1>{props.name}</h1>
+     <h1>{props.age}</h1>
+     <h1>{props.email}</h1>
+    </div>
+  );
+};
+
+const Job = (props) => {
+  return (
+    <div>
+     <h1>{props.salary}</h1>
+     <h1>{props.position}</h1>
+     <h1>{props.company}</h1>
+    </div>
+  );
+};
 
 export default App;
