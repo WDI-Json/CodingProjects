c421744a31891ef7138ad796da0dec4a85f468fb
WDijks <w.dijks@arcady.nl>
2023-04-09 20:39:16 +0200

    module 4 useState

--- webdev/React/react8hours/src/App.js
+++ webdev/React/react8hours/src/App.js
@@ -1,23 +1,44 @@
 import styles from "./App.module.css";
 import { Planet } from "./Planets";
+import { useState } from "react";
 
+// function App() {
+//   const planets = [
+//     { name: "Mars", isGasPlanet: false },
+//     { name: "Earth", isGasPlanet: false },
+//     { name: "Jupiter", isGasPlanet: true },
+//     { name: "Venus", isGasPlanet: false },
+//     { name: "Neptune", isGasPlanet: true },
+//     { name: "Uranus", isGasPlanet: true },
+//   ];
 
+//   return (
+//     <div className={styles.App}>
+//       {planets.map((planet, key) => {
+//         return <Planet name={planet.name} isGasPlanet={planet.isGasPlanet} />
+//       })}
+//     </div>
+//   );
+// }
 function App() {
-  const planets = [
-    { name: "Mars", isGasPlanet: false },
-    { name: "Earth", isGasPlanet: false },
-    { name: "Jupiter", isGasPlanet: true },
-    { name: "Venus", isGasPlanet: false },
-    { name: "Neptune", isGasPlanet: true },
-    { name: "Uranus", isGasPlanet: true },
-  ];
+  const [count, setNumber] = useState(0);
 
   return (
     <div className={styles.App}>
-      {planets.map((planet, key) => {
-        return <Planet name={planet.name} isGasPlanet={planet.isGasPlanet} />
-      })}
-    </div>
+      <button onClick={() => {
+        setNumber(count + 1)
+      }}>Increase</button>
+
+      <button onClick={() => {
+        setNumber(count - 1)
+      }}>Decrease</button>
+
+      <button onClick={() => {
+        setNumber(0)
+      }}>Reset to 0</button>
+
+      <h1>Current number is {count}</h1>
+    </div >
   );
 }
 
