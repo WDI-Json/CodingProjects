71e047529b4672777665762679e017b2832ba7f0
WDijks <w.dijks@arcady.nl>
2023-05-21 20:48:31 +0200

    react queries!

--- webdev/React/react8hours/src/App.js
+++ webdev/React/react8hours/src/App.js
@@ -5,25 +5,34 @@ import { Profile } from "./pages/Profile";
 import { Contact } from "./pages/Contact";
 import { Navbar } from "./pages/Navbar";
 import { useState, createContext } from "react";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 export const AppContext = createContext();
 
 function App() {
   const [userName, setUserName] = useState("Wouter")
+  const client = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false
+      }
+    }
+  })
 
   return (
     <div className={styles.App}>
-      <AppContext.Provider value={{ userName, setUserName }}>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="*" element={<h1> PAGE NOT FOUND</h1>} />
-          </Routes>
-        </Router>
-      </AppContext.Provider>
+      <QueryClientProvider client={client}>
+        <AppContext.Provider value={{ userName, setUserName }}>
+          <Router>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="*" element={<h1> PAGE NOT FOUND</h1>} />
+            </Routes>
+          </Router>
+        </AppContext.Provider>
+      </QueryClientProvider>
     </div>
   );
 }
--- webdev/React/react8hours/src/pages/Home.js
+++ webdev/React/react8hours/src/pages/Home.js
@@ -1,7 +1,28 @@
-import { useContext } from "react"
-import { AppContext } from "../App"
+import { isError, useQuery } from "@tanstack/react-query"
+import Axios from "axios";
 
 export const Home = () => {
-    const { userName } = useContext(AppContext)
-    return <h1> This is homepage. loggedInUser: {userName} </h1>
+    const {
+        data: catData,
+        isLoading,
+        isError,
+        refetch
+    } = useQuery(["cat"], () => {
+        return Axios.get("https://catfact.ninja/fact").then((res) => res.data)
+    });
+
+    if (isError) {
+        return <h1>ERROR NO CAT FOR U</h1>
+    }
+
+    if (isLoading) {
+        return <h1>LOADING CATFACT</h1>
+    }
+    return (
+        <h1>
+            This is homepage.
+            <p>{catData?.fact}</p>
+            <button onClick={refetch}>UPDATE DATA</button>
+        </h1>
+    )
 }
