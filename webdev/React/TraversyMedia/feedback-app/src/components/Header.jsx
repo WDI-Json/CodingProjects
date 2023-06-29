46e52e0e69296f4677f53a2c9e6c335b5ba03c71
Wouter Dijks <wouterdijks@hotmail.com>
2023-06-29 19:43:11 +0200

    adding styles

--- webdev/React/TraversyMedia/feedback-app/src/components/Header.jsx
+++ webdev/React/TraversyMedia/feedback-app/src/components/Header.jsx
@@ -1,20 +1,28 @@
-import PropTypes from "prop-types";
+import PropTypes from "prop-types"
 
-function Header({ text }) {
+function Header({ text, bgColor, textColor }) {
+  const headerStyles = {
+    backgroundColor: bgColor,
+    color: textColor,
+  }
   return (
-    <header>
+    <header style={headerStyles}>
       <div className="container"></div>
       <h2>{text}</h2>
     </header>
-  );
+  )
 }
 
 Header.defaultProps = {
   text: "Feedback UI",
-};
+  bgColor: "rgba(0,0,0,0.4)",
+  textColor: "ff6a95",
+}
 
 Header.propTypes = {
   text: PropTypes.string,
-};
+  bgColor: PropTypes.string,
+  textColor: PropTypes.string,
+}
 
-export default Header;
+export default Header
