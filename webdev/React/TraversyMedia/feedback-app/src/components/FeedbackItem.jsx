94efcb9e3eda10287d66511ca3b20b6592f168e9
Wouter Dijks <wouterdijks@hotmail.com>
2023-06-30 09:16:34 +0200

    data added and three components visibile

--- webdev/React/TraversyMedia/feedback-app/src/components/FeedbackItem.jsx
+++ webdev/React/TraversyMedia/feedback-app/src/components/FeedbackItem.jsx
@@ -1,19 +1,10 @@
 import React, { useState } from "react"
 
-function FeedbackItem() {
-  const [rating, setRating] = useState(10)
-  const [text, setText] = useState(
-    `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, molestias
-    doloremque? Necessitatibus architecto, iste, expedita laborum
-    voluptatibus ipsum inventore repellendus ratione adipisci esse mollitia
-    voluptatem, fugit placeat pariatur animi atque? 
-    `
-  )
-
+function FeedbackItem({ item }) {
   return (
     <div className="card">
-      <div className="num-display">{rating}</div>
-      <div className="text-display">{text}</div>
+      <div className="num-display">{item.rating}</div>
+      <div className="text-display">{item.text}</div>
     </div>
   )
 }
---
+++ webdev/React/TraversyMedia/feedback-app/src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur atque laudantium omnis quas quisquam sed qui, alias magnam natus debitis. Impedit eos, quibusdam hic quod quis reiciendis rerum enim officia?",
+  },
+  {
+    id: 2,
+    rating: 7,
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur atque laudantium omnis quas quisquam sed qui, alias magnam natus debitis. Impedit eos, quibusdam hic quod quis reiciendis rerum enim officia?",
+  },
+  {
+    id: 3,
+    rating: 9,
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur atque laudantium omnis quas quisquam sed qui, alias magnam natus debitis. Impedit eos, quibusdam hic quod quis reiciendis rerum enim officia?",
+  },
+]
+
+export default FeedbackData
