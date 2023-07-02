ffef40c5a75d2b75e488ce6c4a920a51c738e2c4
Wouter Dijks <wouterdijks@hotmail.com>
2023-07-02 12:18:35 +0200

    functional working

--- webdev/React/TraversyMedia/feedback-app/src/components/FeedbackForm.jsx
+++ webdev/React/TraversyMedia/feedback-app/src/components/FeedbackForm.jsx
@@ -3,7 +3,7 @@ import Card from "./shared/Card"
 import Button from "./shared/Button"
 import RatingSelect from "./RatingSelect"
 
-function FeedbackForm() {
+function FeedbackForm({ handleAdd }) {
   const [text, setText] = useState("")
   const [btnDisabled, setBtnDisabled] = useState(true)
   const [message, setMessage] = useState("")
@@ -23,9 +23,22 @@ function FeedbackForm() {
     setText(e.target.value)
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if (text.trim().length > 10) {
+      const newFeedback = {
+        text,
+        rating,
+      }
+      handleAdd(newFeedback)
+      setText("")
+    }
+  }
+
   return (
     <Card>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>How would you rate your service with us?</h2>
         <RatingSelect select={(rating) => setRating(rating)} />
         <div className="div input-group">
@@ -38,8 +51,8 @@ function FeedbackForm() {
           <Button type="submit" isDisabled={btnDisabled}>
             Send
           </Button>
-          {message && <div className="message">{message}</div>}
         </div>
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   )
