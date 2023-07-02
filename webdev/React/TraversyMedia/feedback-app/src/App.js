2c7cc8563e3832fe6394bc8091b57d6aae129589
Wouter Dijks <wouterdijks@hotmail.com>
2023-07-02 14:16:07 +0200

    made start with useContext

--- webdev/React/TraversyMedia/feedback-app/src/App.js
+++ webdev/React/TraversyMedia/feedback-app/src/App.js
@@ -8,6 +8,7 @@ import FeedbackStats from "./components/FeedbackStats"
 import FeedbackForm from "./components/FeedbackForm"
 import About from "./pages/About"
 import AboutIconLink from "./components/AboutIconLink"
+import { FeedbackProvider } from "./context/FeedbackContext"
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
@@ -23,30 +24,32 @@ function App() {
     setFeedback([newFeedback, ...feedback])
   }
   return (
-    <Router>
-      <Header />
-      <div className="container">
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={
-              <>
-                <FeedbackForm handleAdd={addFeedback} />
-                <FeedbackStats feedback={feedback} />
-                <FeedbackList
-                  feedback={feedback}
-                  handleDelete={deleteFeedback}
-                />
-              </>
-            }
-          ></Route>
+    <FeedbackProvider>
+      <Router>
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <>
+                  <FeedbackForm handleAdd={addFeedback} />
+                  <FeedbackStats feedback={feedback} />
+                  <FeedbackList
+                    feedback={feedback}
+                    handleDelete={deleteFeedback}
+                  />
+                </>
+              }
+            ></Route>
 
-          <Route path="/about" element={<About />} />
-        </Routes>
-        <AboutIconLink />
-      </div>
-    </Router>
+            <Route path="/about" element={<About />} />
+          </Routes>
+          <AboutIconLink />
+        </div>
+      </Router>
+    </FeedbackProvider>
   )
 }
 
