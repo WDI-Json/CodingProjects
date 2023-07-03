0d0f9ad236cc1d1bd2b86f2b749f3046e41b6108
Wouter Dijks <wouterdijks@hotmail.com>
2023-07-03 16:16:53 +0200

    finish project

--- webdev/React/TraversyMedia/feedback-app/src/context/FeedbackContext.js
+++ webdev/React/TraversyMedia/feedback-app/src/context/FeedbackContext.js
@@ -16,17 +16,31 @@ export const FeedbackProvider = ({ children }) => {
   }, [])
 
   const fetchFeedback = async () => {
-    const response = await fetch(
-      "http://localhost:3333/feedback?_sort=id&_order=desc"
-    )
+    const response = await fetch("/feedback?_sort=id&_order=desc")
     const data = await response.json()
 
     setFeedback(data)
     setIsLoading(false)
   }
 
-  const deleteFeedback = (id) => {
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch("/feedback", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newFeedback),
+    })
+
+    const data = await response.json()
+
+    setFeedback([data, ...feedback])
+  }
+
+  const deleteFeedback = async (id) => {
     if (window.confirm("Are you sure you want to delete?")) {
+      await fetch(`/feedback/${id}`, { method: "DELETE" })
+
       setFeedback(feedback.filter((item) => item.id !== id))
     }
   }
@@ -37,17 +51,23 @@ export const FeedbackProvider = ({ children }) => {
       edit: true,
     })
   }
-  const updateFeedback = (id, updatedItem) => {
-    setFeedback(
-      feedback.map((item) =>
-        item.id === id ? { ...item, ...updatedItem } : item
-      )
-    )
-  }
 
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4()
-    setFeedback([newFeedback, ...feedback])
+  const updateFeedback = async (id, updItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updItem),
+    })
+
+    const data = await response.json()
+
+    setFeedback(feedback.map((item) => (item.id === id ? data : item)))
+    setFeedbackEdit({
+      item: {},
+      edit: false,
+    })
   }
 
   return (
