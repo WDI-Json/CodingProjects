3d591af6460333ebb39e4769347ceb0bc0e4a7da
Wouter Dijks <wouterdijks@hotmail.com>
2023-07-04 10:24:05 +0200

    basic navbar

--- webdev/React/TraversyMedia/github-finder/src/App.js
+++ webdev/React/TraversyMedia/github-finder/src/App.js
@@ -1,5 +1,15 @@
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import Navbar from "./components/layout/Navbar"
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        <main>Content</main>
+      </div>
+    </Router>
+  )
 }
 
 export default App
