8fc85de46829c1df48d7f1e99590281de8be9a30
Wouter Dijks <wouterdijks@hotmail.com>
2023-07-06 10:57:19 +0200

    UI search functionality

--- webdev/React/TraversyMedia/github-finder/src/context/github/GithubContext.js
+++ webdev/React/TraversyMedia/github-finder/src/context/github/GithubContext.js
@@ -1,4 +1,5 @@
-import { createContext, useEffect, useState } from "react"
+import { createContext, useReducer } from "react"
+import githubReducer from "./GithubReducer"
 
 const GithubContext = createContext()
 
@@ -6,10 +7,15 @@ const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 const GITHUB_TOKEN = "add later"
 
 export const GithubProvider = ({ children }) => {
-  const [users, setUsers] = useState([])
-  const [loading, setLoading] = useState(true)
+  const initialState = {
+    users: [],
+    loading: false,
+  }
+
+  const [state, dispatch] = useReducer(githubReducer, initialState)
 
   const fetchUsers = async () => {
+    setLoading()
     const response = await fetch(
       `${GITHUB_URL}/users`
       //* you did not use a token
@@ -21,15 +27,20 @@ export const GithubProvider = ({ children }) => {
     )
 
     const data = await response.json()
-    setUsers(data)
-    setLoading(false)
+    dispatch({
+      type: "GET_USERS",
+      payload: data,
+    })
   }
 
+  const setLoading = () => {
+    dispatch({ type: "SET_LOADING" })
+  }
   return (
     <GithubContext.Provider
       value={{
-        users,
-        loading,
+        users: state.users,
+        loading: state.loading,
         fetchUsers,
       }}
     >
