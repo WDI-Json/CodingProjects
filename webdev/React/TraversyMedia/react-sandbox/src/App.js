d1e6a0bbcdb48f6fbec470188e64c8daaaeb0c75
Wouter Dijks <wouterdijks@hotmail.com>
2023-07-10 14:51:12 +0200

    usememo and usecallback example

--- webdev/React/TraversyMedia/react-sandbox/src/App.js
+++ webdev/React/TraversyMedia/react-sandbox/src/App.js
@@ -1,13 +1,17 @@
-import UseRefExample1 from "./components/UseRefExample1";
-import UseRefExample2 from "./components/UseRefExample2";
-import UseRefExample3 from "./components/UseRefExample3";
+import UseCallbackExample from "./components/UseCallbackExample";
+// import UseMemoExample from "./components/UseMemoExample";
+// import UseRefExample1 from "./components/UseRefExample1";
+// import UseRefExample2 from "./components/UseRefExample2";
+// import UseRefExample3 from "./components/UseRefExample3";
 
 function App() {
   return (
     <div className="container mt-5">
       {/* <UseRefExample1 /> */}
       {/* <UseRefExample2 /> */}
-      <UseRefExample3 />
+      {/* <UseRefExample3 /> */}
+      {/* <UseMemoExample /> */}
+      <UseCallbackExample />
     </div>
   );
 }
