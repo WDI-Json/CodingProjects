b7920551d82834690cc5d1f256778c85de0de301
Wouter Dijks <wouterdijks@hotmail.com>
2023-07-13 10:53:06 +0200

    offers page

--- webdev/React/TraversyMedia/house-marketplace/src/App.js
+++ webdev/React/TraversyMedia/house-marketplace/src/App.js
@@ -1,11 +1,16 @@
+// Libraries
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { ToastContainer } from "react-toastify"
+
+// Pages
 import Explore from "./pages/Explore"
 import Offers from "./pages/Offers"
 import Profile from "./pages/Profile"
 import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import ForgotPassword from "./pages/ForgotPassword"
+import Category from "./pages/Category"
+// Components
 import Navbar from "./components/Navbar"
 import PrivateRoute from "./components/PrivateRoute"
 
@@ -16,6 +21,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Explore />} />
           <Route path="/offers" element={<Offers />} />
+          <Route path="/category/:categoryName" element={<Category />} />
           <Route path="/profile" element={<PrivateRoute />}>
             <Route path="/profile" element={<Profile />} />
           </Route>
