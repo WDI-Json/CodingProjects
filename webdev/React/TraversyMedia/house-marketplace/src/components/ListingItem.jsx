ce2fa80f243ba77f253110d89a1b5c0e8ec9278d
Wouter Dijks <wouterdijks@hotmail.com>
2023-07-17 09:34:48 +0200

    added edit listing and changed some css

--- webdev/React/TraversyMedia/house-marketplace/src/components/ListingItem.jsx
+++ webdev/React/TraversyMedia/house-marketplace/src/components/ListingItem.jsx
@@ -1,9 +1,10 @@
-import { Link } from "react-router-dom"
-import { ReactComponent as DeleteIcon } from "../assets/svg/deleteIcon.svg"
-import bedIcon from "../assets/svg/bedIcon.svg"
-import bathtubIcon from "../assets/svg/bathtubIcon.svg"
+import { Link } from "react-router-dom";
+import { ReactComponent as DeleteIcon } from "../assets/svg/deleteIcon.svg";
+import { ReactComponent as EditIcon } from "../assets/svg/editIcon.svg";
+import bedIcon from "../assets/svg/bedIcon.svg";
+import bathtubIcon from "../assets/svg/bathtubIcon.svg";
 
-function ListingItem({ listing, id, onDelete }) {
+function ListingItem({ listing, id, onDelete, onEdit }) {
   return (
     <li className="categoryListing">
       <Link
@@ -42,18 +43,24 @@ function ListingItem({ listing, id, onDelete }) {
                 ? `${listing.bathrooms} Bathrooms`
                 : "1 Bathroom"}
             </p>
+            <div className="editAndDelete">
+              {onDelete && (
+                <DeleteIcon
+                  className="removeIcon"
+                  fill="rgb(231,76,60)"
+                  onClick={() => onDelete(listing.id, listing.name)}
+                />
+              )}
+
+              {onEdit && (
+                <EditIcon className="editIcon" onClick={() => onEdit(id)} />
+              )}
+            </div>
           </div>
         </div>
       </Link>
-      {onDelete && (
-        <DeleteIcon
-          className="removeIcon"
-          fill="rgb(231,76,60)"
-          onClick={() => onDelete(listing.id, listing.name)}
-        />
-      )}
     </li>
-  )
+  );
 }
 
-export default ListingItem
+export default ListingItem;
