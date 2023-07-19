a45323a7c485745ed6dc4351d87084d254236386
Wouter Dijks <wouterdijks@hotmail.com>
2023-07-19 21:50:05 +0200

    added express async handler

--- webdev/React/TraversyMedia/support-desk/backend/server.js
+++ webdev/React/TraversyMedia/support-desk/backend/server.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const dotenv = require("dotenv").config();
-const { errorHandler } = require("./middleware/errorMiddleware");
-const PORT = process.env.PORT || 5000;
-const app = express();
+const express = require("express")
+const dotenv = require("dotenv").config()
+const { errorHandler } = require("./middleware/errorMiddleware")
+const PORT = process.env.PORT || 5555
+const app = express()
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "hello welcome to supportdesk api" });
-});
+  res.status(200).json({ message: "hello welcome to supportdesk api" })
+})
 
 //Routes
-app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/users", require("./routes/userRoutes"))
 
-app.use(errorHandler);
+app.use(errorHandler)
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
