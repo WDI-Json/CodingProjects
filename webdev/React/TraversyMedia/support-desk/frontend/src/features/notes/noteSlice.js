9590fc9010f6246c043044e0f4ae5740976f3852
Wouter Dijks <wouterdijks@hotmail.com>
2023-07-22 15:20:13 +0200

    fetch notes

---
+++ webdev/React/TraversyMedia/support-desk/frontend/src/features/notes/noteSlice.js
@@ -0,0 +1,50 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import noteService from "./noteService";
+import { extractErrorMessage } from "../../utils";
+
+const initialState = {
+  notes: [],
+  isError: false,
+  isSucces: false,
+  isLoading: false,
+  message: "",
+};
+
+export const getNotes = createAsyncThunk(
+  "notes/getAll",
+  async (ticketId, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token;
+      return await noteService.createNote(ticketId, token);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(extractErrorMessage(error));
+    }
+  }
+);
+
+export const noteSlice = createSlice({
+  name: "note",
+  initialState,
+  reducers: {
+    reset: (state) => initialState,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getNotes.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getNotes.fulfilled, (state, action) => {
+        state.notes = action.payload;
+        state.isLoading = false;
+        state.isSucces = true;
+      })
+      .addCase(getNotes.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      });
+  },
+});
+
+export const { reset } = noteSlice.actions;
+export default noteSlice.reducer;
