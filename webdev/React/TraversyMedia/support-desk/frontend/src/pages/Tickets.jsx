c4b44eb7d83730f6b4fd080eebc26c622932214f
Wouter Dijks <wouterdijks@hotmail.com>
2023-07-22 13:42:48 +0200

    added ticketView and getAll

--- webdev/React/TraversyMedia/support-desk/frontend/src/pages/Tickets.jsx
+++ webdev/React/TraversyMedia/support-desk/frontend/src/pages/Tickets.jsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { getTickets, reset } from "../features/tickets/ticketSlice";
 import Spinner from "../components/Spinner";
 import BackButton from "../components/BackButton";
+import TicketItem from "../components/TicketItem";
 
 function Tickets() {
   const { user } = useSelector((state) => state.auth);
@@ -26,6 +27,22 @@ function Tickets() {
     <Spinner />;
   }
 
-  return <div>Tickets</div>;
+  return (
+    <>
+      <BackButton url="/" />
+      <h1>Tickets</h1>
+      <div className="tickets">
+        <div className="ticket-headings">
+          <div>Date</div>
+          <div>Product</div>
+          <div>Status</div>
+          <div></div>
+        </div>
+        {tickets.map((ticket) => (
+          <TicketItem key={ticket.id} ticket={ticket} />
+        ))}
+      </div>
+    </>
+  );
 }
 export default Tickets;
