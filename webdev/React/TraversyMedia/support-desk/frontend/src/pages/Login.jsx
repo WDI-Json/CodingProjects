be95c0b710e5112a3ec274ff5fd9c6970306ef55
Wouter Dijks <wouterdijks@hotmail.com>
2023-07-25 11:41:45 +0200

    unused var

--- webdev/React/TraversyMedia/support-desk/frontend/src/pages/Login.jsx
+++ webdev/React/TraversyMedia/support-desk/frontend/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import { toast } from "react-toastify"
 import { FaSignInAlt } from "react-icons/fa"
 import { useNavigate } from "react-router-dom"
