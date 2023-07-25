4e5d1679a042499fe441776d105ef7ebf47fdcb0
Wouter Dijks <wouterdijks@hotmail.com>
2023-07-25 10:18:14 +0200

    noteslice refactor and cleanup

--- webdev/React/TraversyMedia/support-desk/frontend/src/features/notes/noteSlice.js
+++ webdev/React/TraversyMedia/support-desk/frontend/src/features/notes/noteSlice.js
@@ -1,78 +1,52 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import noteService from "./noteService";
-import { extractErrorMessage } from "../../utils";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import noteService from "./noteService"
+import { extractErrorMessage } from "../../utils"
 
 const initialState = {
   notes: [],
-  isError: false,
-  isSucces: false,
-  isLoading: false,
-  message: "",
-};
+}
 
 export const getNotes = createAsyncThunk(
   "notes/getAll",
   async (ticketId, thunkAPI) => {
     try {
-      const token = thunkAPI.getState().auth.user.token;
-      return await noteService.getNotes(ticketId, token);
+      const token = thunkAPI.getState().auth.user.token
+      return await noteService.getNotes(ticketId, token)
     } catch (error) {
-      return thunkAPI.rejectWithValue(extractErrorMessage(error));
+      return thunkAPI.rejectWithValue(extractErrorMessage(error))
     }
   }
-);
+)
 
 export const createNote = createAsyncThunk(
   "notes/create",
   async ({ noteText, ticketId }, thunkAPI) => {
     try {
-      const token = thunkAPI.getState().auth.user.token;
-      return await noteService.createNote(noteText, ticketId, token);
+      const token = thunkAPI.getState().auth.user.token
+      return await noteService.createNote(noteText, ticketId, token)
     } catch (error) {
-      return thunkAPI.rejectWithValue(extractErrorMessage(error));
+      return thunkAPI.rejectWithValue(extractErrorMessage(error))
     }
   }
-);
+)
 
 export const noteSlice = createSlice({
   name: "note",
   initialState,
-  reducers: {
-    reset: (state) => initialState,
-  },
   extraReducers: (builder) => {
     builder
-      // GET ALL
       .addCase(getNotes.pending, (state) => {
-        state.isLoading = true;
+        // NOTE: reset notes to null on pending so we can show a Spinner while fetching notes
+        state.notes = null
       })
       .addCase(getNotes.fulfilled, (state, action) => {
-        state.notes = action.payload;
-        state.isLoading = false;
-        state.isSucces = true;
-      })
-      .addCase(getNotes.rejected, (state, action) => {
-        state.isLoading = false;
-        state.isError = true;
-        state.message = action.payload;
-      })
-      // CREATE NOTE
-      .addCase(createNote.pending, (state) => {
-        state.isLoading = true;
+        // Possible empty array
+        state.notes = action.payload
       })
       .addCase(createNote.fulfilled, (state, action) => {
-        //with help of redux toolkit this is possible
-        state.notes.push(action.payload);
-        state.isLoading = false;
-        state.isSucces = true;
+        state.notes.push(action.payload)
       })
-      .addCase(createNote.rejected, (state, action) => {
-        state.isLoading = false;
-        state.isError = true;
-        state.message = action.payload;
-      });
   },
-});
+})
 
-export const { reset } = noteSlice.actions;
-export default noteSlice.reducer;
+export default noteSlice.reducer
