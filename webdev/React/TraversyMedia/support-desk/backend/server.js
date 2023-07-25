55ab4d6115c3b86abc48d9cc6adf15d1054e1948
Wouter Dijks <wouterdijks@hotmail.com>
2023-07-25 09:45:12 +0200

    static build file

--- webdev/React/TraversyMedia/support-desk/backend/server.js
+++ webdev/React/TraversyMedia/support-desk/backend/server.js
@@ -1,25 +1,35 @@
-const express = require("express");
-const colors = require("colors");
-const dotenv = require("dotenv").config();
-const { errorHandler } = require("./middleware/errorMiddleware");
-const connectDB = require("./config/db");
-const PORT = process.env.PORT || 5000;
-const app = express();
+const express = require("express")
+require("colors")
+require("dotenv").config()
+const { errorHandler } = require("./middleware/errorMiddleware")
+const connectDB = require("./config/db")
+const PORT = process.env.PORT || 5000
+const app = express()
 
 // Connect to DB
-connectDB();
+connectDB()
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "hello welcome to supportdesk api" });
-});
+// Routes
+app.use("/api/users", require("./routes/userRoutes"))
+app.use("/api/tickets", require("./routes/ticketRoutes"))
 
-//Routes
-app.use("/api/users", require("./routes/userRoutes"));
-app.use("/api/tickets", require("./routes/ticketRoutes"));
+// Serve Frontend
+if (process.env.NODE_ENV === "production") {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, "../frontend/build")))
 
-app.use(errorHandler);
+  // FIX: below code fixes app crashing on refresh in deployment
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"))
+  })
+} else {
+  app.get("/", (_, res) => {
+    res.status(200).json({ message: "Welcome to the Support Desk API" })
+  })
+}
+app.use(errorHandler)
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
