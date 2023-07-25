49157b04d70207a576cb0687f7ef20cc85c462a9
Wouter Dijks <wouterdijks@hotmail.com>
2023-07-25 16:41:32 +0200

    input is now always lowercased

--- webdev/React/TraversyMedia/support-desk/frontend/src/utils.js
+++ webdev/React/TraversyMedia/support-desk/frontend/src/utils.js
@@ -1,3 +1,23 @@
 export function extractErrorMessage(error) {
-  return error.response?.data?.message || error.message || error.toString();
+  return error.response?.data?.message || error.message || error.toString()
 }
+
+// Function to recursively convert all string values to lowercase
+export function objectStringsToLowercase(obj) {
+  const newObj = obj
+
+  for (const key in newObj) {
+    const value = obj[key]
+    if (typeof value === "string") {
+      newObj[key] = value.toLowerCase()
+    }
+  }
+  return newObj
+}
+
+const utils = {
+  extractErrorMessage,
+  objectStringsToLowercase,
+}
+
+export default utils
