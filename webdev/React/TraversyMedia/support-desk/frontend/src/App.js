e1b8eede7c3e06e717ce757355a5de703ff281a4
Wouter Dijks <wouterdijks@hotmail.com>
2023-07-25 09:53:27 +0200

    no need for useAuthStatus since duplicate state

--- webdev/React/TraversyMedia/support-desk/frontend/src/App.js
+++ webdev/React/TraversyMedia/support-desk/frontend/src/App.js
@@ -1,15 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 //pages
-import Home from "./pages/Home";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
-import Header from "./components/Header";
-import NewTicket from "./pages/NewTicket";
-import PrivateRoute from "./components/PrivateRoute";
-import Tickets from "./pages/Tickets";
-import Ticket from "./pages/Ticket";
+import Home from "./pages/Home"
+import Register from "./pages/Register"
+import Login from "./pages/Login"
+import Header from "./components/Header"
+import NewTicket from "./pages/NewTicket"
+import PrivateRoute from "./components/PrivateRoute"
+import Tickets from "./pages/Tickets"
+import Ticket from "./pages/Ticket"
 
 function App() {
   return (
@@ -22,22 +22,38 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
 
-            <Route path="/new-ticket" element={<PrivateRoute />}>
-              <Route path="/new-ticket" element={<NewTicket />} />
-            </Route>
-            <Route path="/tickets" element={<PrivateRoute />}>
-              <Route path="/tickets" element={<Tickets />} />
-            </Route>
+            <Route
+              path="/new-ticket"
+              element={
+                <PrivateRoute>
+                  <NewTicket />
+                </PrivateRoute>
+              }
+            />
 
-            <Route path="/ticket/:ticketId" element={<PrivateRoute />}>
-              <Route path="/ticket/:ticketId" element={<Ticket />} />
-            </Route>
+            <Route
+              path="/tickets"
+              element={
+                <PrivateRoute>
+                  <Tickets />
+                </PrivateRoute>
+              }
+            />
+
+            <Route
+              path="/ticket/:ticketId"
+              element={
+                <PrivateRoute>
+                  <Ticket />
+                </PrivateRoute>
+              }
+            />
           </Routes>
         </div>
       </Router>
       <ToastContainer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- webdev/React/TraversyMedia/support-desk/frontend/src/components/PrivateRoute.jsx
+++ webdev/React/TraversyMedia/support-desk/frontend/src/components/PrivateRoute.jsx
@@ -1,14 +1,10 @@
-import { Navigate, Outlet } from "react-router-dom";
-import { useAuthStatus } from "../hooks/useAuthStatus";
-import Spinner from "./Spinner";
+import { Navigate } from "react-router-dom"
+import { UseSelector, useSelector } from "react-redux"
 
-const PrivateRoute = () => {
-  const { loggedIn, checkingStatus } = useAuthStatus();
+const PrivateRoute = ({ children }) => {
+  const { user } = useSelector((state) => state.auth)
 
-  if (checkingStatus) {
-    return <Spinner />;
-  }
+  return user ? children : <Navigate to="/login" />
+}
 
-  return loggedIn ? <Outlet /> : <Navigate to="/login" />;
-};
-export default PrivateRoute;
+export default PrivateRoute
