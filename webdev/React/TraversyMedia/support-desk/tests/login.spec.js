82fa0744d76ee8f74698917467c00acc3dba659b
Wouter Dijks <wouterdijks@hotmail.com>
2023-07-25 16:43:06 +0200

    made regression test for input

--- webdev/React/TraversyMedia/support-desk/tests/login.spec.js
+++ webdev/React/TraversyMedia/support-desk/tests/login.spec.js
@@ -1,36 +1,66 @@
 const { test, expect } = require("@playwright/test")
+import { setTimeout } from "timers/promises"
 
-test.beforeEach(async ({ page }) => {
-  await page.goto("http://localhost:3000/")
-})
-
+const landingPage = "http://localhost:3000/"
 const testUser = {
-  email: "Wouter",
+  email: "wouter@mail.com",
   password: "123456",
 }
 
-test.describe("Navigating the site", () => {
+test.beforeEach(async ({ page }, testInfo) => {
+  console.log(`Running ${testInfo.title}`)
+  await page.goto(landingPage)
+})
+
+test.describe("Accessibility", () => {
   test("Has title", async ({ page }) => {
-    await page.goto("http://localhost:3000/")
-
+    await page.goto(landingPage)
     await expect(page).toHaveTitle("Support Desk")
-  })
-
-  test("", async ({ page }) => {
-    await page.goto("http://localhost:3000/login")
-
-    await expect(page).toHaveURL(/login/)
   })
 })
 
-test.describe("Authentication", () => {
-  test("Logging in", async ({ page }) => {
-    await page.goto("http://localhost:3000/")
+test.describe("Login", () => {
+  test("Land through URL", async ({ page }) => {
+    await page.goto(landingPage + "login")
+    expect(page.url()).toBe(landingPage + "login")
+  })
+
+  test("Land by button", async ({ page }) => {
+    await page.getByRole("link", { name: "Login" }).click()
+    expect(page.url()).toBe(landingPage + "login")
+  })
+
+  test("Successful lowercase", async ({ page }) => {
+    await page.goto(landingPage)
     await page.getByRole("link", { name: "Login" }).click()
     await page.getByPlaceholder("Enter your email").fill(testUser.email)
     await page.getByPlaceholder("Enter your password").fill(testUser.password)
     await page.getByRole("button", { name: "Submit" }).click()
+    await page.waitForURL(landingPage)
+    await expect(page.url()).toBe(landingPage)
+  })
 
-    await expect(page).toHaveURL(/login/)
+  test("Successful uppercase", async ({ page }) => {
+    await page.goto(landingPage)
+    await page.getByRole("link", { name: "Login" }).click()
+    await page
+      .getByPlaceholder("Enter your email")
+      .fill(testUser.email.toUpperCase())
+    await page.getByPlaceholder("Enter your password").fill(testUser.password)
+    await page.getByRole("button", { name: "Submit" }).click()
+    await page.waitForURL(landingPage)
+    await expect(page.url()).toBe(landingPage)
+  })
+
+  test("Failed", async ({ page }) => {
+    await page.goto(landingPage)
+    await page.getByRole("link", { name: "Login" }).click()
+    await page.getByPlaceholder("Enter your email").fill(testUser.email)
+    await page
+      .getByPlaceholder("Enter your password")
+      .fill(testUser.password + "gibberish")
+    await page.getByRole("button", { name: "Submit" }).click()
+    await setTimeout(500)
+    expect(page.url()).toBe(landingPage + "login")
   })
 })
