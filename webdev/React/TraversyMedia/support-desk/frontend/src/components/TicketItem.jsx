0f0c74983e10bf8ea64d5075171a421ba28b4694
Wouter Dijks <wouterdijks@hotmail.com>
2023-07-25 13:00:15 +0200

    cleanup ticketItem

--- webdev/React/TraversyMedia/support-desk/frontend/src/components/TicketItem.jsx
+++ webdev/React/TraversyMedia/support-desk/frontend/src/components/TicketItem.jsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom"
 
 function TicketItem({ ticket }) {
+  const { createdAt, product, status, _id } = ticket
   return (
     <tr className="ticket">
-      <td>{new Date(ticket.createdAt).toLocaleString("en-gb")}</td>
-      <td>{ticket.product}</td>
-      <td className={`status status-${ticket.status}`}>{ticket.status}</td>
+      <td>{new Date(createdAt).toLocaleString("en-gb")}</td>
+      <td>{product}</td>
+      <td className={`status status-${status}`}>{status}</td>
       <td>
-        <Link to={`/ticket/${ticket._id}`} className="btn btn-reverse btn-sm">
+        <Link to={`/ticket/${_id}`} className="btn btn-reverse btn-sm">
           View
         </Link>
       </td>
