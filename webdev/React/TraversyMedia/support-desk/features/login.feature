475c798de59c992ded30c6c08290c5e1aa27f969
Wouter Dijks <wouterdijks@hotmail.com>
2023-07-28 11:38:15 +0200

    working test!

--- webdev/React/TraversyMedia/support-desk/features/login.feature
+++ webdev/React/TraversyMedia/support-desk/features/login.feature
@@ -4,7 +4,7 @@ Rule: correct credentials are entered
 
     Scenario: Bob wants to login
         Given Bob opens support-desk website
-        And navigates to /login
+        And goes to loginpage
         When he has filled in correct credentials
         Then Bob is navigated to the homepage 
         And is able to view his tickets
--- webdev/React/TraversyMedia/support-desk/features/steps.js
+++ webdev/React/TraversyMedia/support-desk/features/steps.js
@@ -1,4 +1,5 @@
 const { Given, When, Then } = require("@cucumber/cucumber")
+const { test, expect } = require("@playwright/test")
 
 const landingPage = "http://localhost:3000/"
 const testUser = {
@@ -6,39 +7,26 @@ const testUser = {
   password: "123456",
 }
 
-// ? Given Bob opens support-desk website
-// Undefined. Implement with the following snippet:
-
 Given("Bob opens support-desk website", async function () {
-  await page.goto(landingPage)
+  await this.openUrl(landingPage)
 })
 
-// ? And navigates to /login
-// Undefined. Implement with the following snippet:
-
-Given("navigates to \\/login", async function () {
-  await page.getByRole("link", { name: "Login" }).click()
+Given("goes to loginpage", async function () {
+  await this.page.getByRole("link", { name: "Login" }).click()
 })
-
-// ? When he has filled in correct credentials
-// Undefined. Implement with the following snippet:
 
 When("he has filled in correct credentials", async function () {
-  await page.getByPlaceholder("Enter your email").fill(email)
-  await page.getByPlaceholder("Enter your password").fill(password)
+  await this.page.getByPlaceholder("Enter your email").fill(testUser.email)
+  await this.page
+    .getByPlaceholder("Enter your password")
+    .fill(testUser.password)
 })
-
-// ? Then Bob is navigated to the homepage
-// Undefined. Implement with the following snippet:
 
 Then("Bob is navigated to the homepage", async function () {
-  await page.getByRole("button", { name: "Submit" }).click()
+  await this.page.getByRole("button", { name: "Submit" }).click()
 })
 
-// ? And is able to view his tickets
-// Undefined. Implement with the following snippet:
-
-Then("is able to view his tickets", function () {
-  // Write code here that turns the phrase above into concrete actions
-  return "pending"
+Then("is able to view his tickets", async function () {
+  await this.page.getByRole("link", { name: "View my Tickets" }).click()
+  expect(this.page.url()).toBe(landingPage + "tickets")
 })
