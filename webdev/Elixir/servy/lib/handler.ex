04ed24ff53ea693db8c7dcd48f6a96821a157ced
Wouter Dijks <wouterdijks@hotmail.com>
2024-01-29 20:10:42 +0100

    finished vid 7

--- webdev/Elixir/servy/lib/handler.ex
+++ webdev/Elixir/servy/lib/handler.ex
@@ -7,9 +7,7 @@ defmodule Servy.Handler do
     |> format_response
   end
 
-  def log(conv) do
-    IO.inspect conv
-  end
+  def log(conv), do: IO.inspect conv
 
   def parse(request) do
     [method, path, _] = request
@@ -20,27 +18,108 @@ defmodule Servy.Handler do
      %{
       method: method,
       path: path,
-      resp_body: "" }
+      resp_body: "",
+      status: nil
+    }
   end
 
   def route(conv) do
-    %{ conv | resp_body: "Bears, Lions, Tigers" }
+    route(conv, conv.method, conv.path)
   end
 
+  def route(conv, "GET", "/wildthings") do
+    %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
+  end
+
+  def route(conv, "GET", "/bears") do
+    %{conv | status: 200, resp_body: "Teddy, Smokey, Paddington"}
+  end
+
+  def route(conv, "GET", "/bears/" <> id) do
+    %{conv | status: 200, resp_body: "bear #{id}"}
+  end
+
+  def route(conv, "DELETE", "/bears/" <> id) do
+    %{conv | status: 403, resp_body: "bear #{id} is forbidden!"}
+  end
+
+  def route(conv, _method, path) do
+    %{conv | status: 404, resp_body: "No #{path} here!"}
+  end
   def format_response(conv) do
 
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
     Content-Length: #{byte_size(conv.resp_body)}
 
     #{conv.resp_body}
     """
   end
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
+  end
+
 end
 
 request = """
 GET /wildthings HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+DELETE /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /bigfoot HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
