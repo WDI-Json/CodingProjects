761aeb4ff7955253665dcf01de061b0689972139
Wouter Dijks <wouterdijks@hotmail.com>
2024-02-03 17:02:05 +0100

    finished vid 9

--- webdev/Elixir/servy/lib/servy/handler.ex
+++ webdev/Elixir/servy/lib/servy/handler.ex
@@ -52,8 +52,7 @@ defmodule Servy.Handler do
     conv
   end
 
-  def track(%{status: "404", path: path} = conv) do
-    IO.puts("Warning: #{path} is not recognized as a valid endpoint")
+  def track(%{status: 404, path: path} = conv) do
     Logger.warning(
       "Warning: ⚠️⚠️⚠️ #{path} is not recognized as a valid endpoint⚠️⚠️⚠️",
       [
@@ -71,26 +70,59 @@ defmodule Servy.Handler do
   end
 
   def route(%{ method: "GET", path: "/wildthings"} = conv) do
-    %{ conv | status: "200", resp_body: "Bears, Lions, Tigers"}
+    %{ conv | status: 200, resp_body: "Bears, Lions, Tigers"}
   end
 
   def route(%{ method: "GET", path: "/bears"} = conv) do
-    %{ conv | status: "200", resp_body: "Teddy, Smokey, Paddington"}
+    %{ conv | status: 200, resp_body: "Teddy, Smokey, Paddington"}
+  end
+
+  def route(%{ method: "GET", path: "/bears/new"} = conv) do
+    Path.expand("../../pages", __DIR__)
+    |> Path.join("form.html")
+    |> File.read
+    |> handle_file(conv)
   end
 
   def route(%{ method: "GET", path: "/bears/" <> id} = conv) do
-    %{ conv | status: "200", resp_body: "bear #{id}"}
+    %{ conv | status: 200, resp_body: "bear #{id}"}
   end
 
   def route(%{ method: "DELETE", path: "/bears/" <> _id} = conv) do
-    %{ conv | status: "403", resp_body: "Bears must never be deleted!"}
+    %{ conv | status: 403, resp_body: "Bears must never be deleted!"}
+  end
+
+  def route(%{ method: "GET", path: "/about"} = conv) do
+    Path.expand("../../pages", __DIR__)
+    |> Path.join("about.html")
+    |> File.read
+    |> handle_file(conv)
   end
 
   def route(%{ path: path} = conv) do
-    %{ conv | status: "404", resp_body: "No #{path} here!"}
+    %{ conv | status: 404, resp_body: "No #{path} here!"}
   end
 
-  def emojify(%{status: "200"} = conv) do
+  def route(%{ method: "GET", path: "/pages/" <> file} = conv) do
+    Path.expand("../../pages", __DIR__)
+    |> Path.join(file <> ".html")
+    |> File.read
+    |> handle_file(conv)
+  end
+
+  def handle_file({:ok, content}, conv) do
+    %{ conv | status: 200, resp_body: content}
+  end
+
+  def handle_file({:error, :enoent}, conv) do
+    %{ conv | status: 404, resp_body: "File not found!"}
+  end
+
+  def handle_file({:error, reason}, conv) do
+    %{ conv | status: 500, resp_body: "File error: #{reason}"}
+  end
+
+  def emojify(%{status: 200} = conv) do
     emojies = String.duplicate("🚀", 5)
     body = emojies <> "\n" <> conv.resp_body <> "\n" <> emojies
 
@@ -123,12 +155,12 @@ defmodule Servy.Handler do
 
   defp status_reason(code) do
     %{
-      "200" => "OK",
-      "201" => "Created",
-      "401" => "Unauthorized",
-      "403" => "Forbidden",
-      "404" => "Not Found",
-      "500" => "Internal Server Error"
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
     }[code]
   end
 
@@ -196,6 +228,30 @@ IO.puts response
 
 request = """
 GET /wildlife HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /about HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /bears/new HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
