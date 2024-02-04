644d141f1f16e37cf9ad5621d1f6e01fd83f3b93
Wouter Dijks <wouterdijks@hotmail.com>
2024-02-04 16:13:44 +0100

    finished vid 12

--- webdev/Elixir/servy/lib/servy/handler.ex
+++ webdev/Elixir/servy/lib/servy/handler.ex
@@ -1,11 +1,14 @@
 defmodule Servy.Handler do
 
+  alias Servy.Conv
   @moduledoc """
   Handles HTTP requests.
   """
-  require Logger
+  import Servy.Plugins, only: [rewrite_path: 1, log_start: 1, log_progress: 1, track: 1]
+  import Servy.Parser, only: [parse: 1]
+  import Servy.FileHandler, only: [handle_file: 2]
 
-  @pages_path Path.expand("../../pages", __DIR__)
+  @pages_path Path.expand("pages", File.cwd!)
   @doc """
   Transforms the request into a response.
   """
@@ -22,125 +25,57 @@ defmodule Servy.Handler do
     |> format_response_body
   end
 
-  def parse(request) do
-    [method, path, _] = request
-    |> String.split("\n")
-    |> List.first
-    |> String.split(" ")
-
-     %{
-      method: method,
-      path: path,
-      resp_body: "",
-      resp_header: "",
-      status: nil
-    }
-  end
-
-  def rewrite_path(%{path: "/wildlife"} = conv) do
-    %{ conv | method: "GET", path: "/wildthings"}
-  end
-
-  def rewrite_path(%{path: path} = conv) do
-    regex = ~r{\/(?<thing>\w+)\?id=(?<id>\d+)}
-    captures = Regex.named_captures(regex, path)
-    rewrite_path_captures(conv, captures)
-  end
-
-  def rewrite_path(conv), do: conv
-
-  def rewrite_path_captures(conv, %{"thing" => thing, "id" => id}) do
-    %{ conv| path: "/#{thing}/#{id}}" }
-  end
-
-  def rewrite_path_captures(conv, nil), do: conv
-
-  def log_start(conv) do
-    IO.inspect conv
-    Logger.info("#{conv.path} received. \nchecking for bad input🔍🔍🔍")
-    conv
-  end
-
-  def track(%{status: 404, path: path} = conv) do
-    Logger.warning(
-      "Warning: ⚠️⚠️⚠️ #{path} is not recognized as a valid endpoint⚠️⚠️⚠️",
-      [
-        date: DateTime.utc_now(),
-        request_id: to_string(:erlang.ref_to_list(:erlang.make_ref()))
-        ])
-    conv
-  end
-
-  def track(conv), do: conv
-
-  def log_progress(conv) do
-    Logger.info("Pre-checks succes✅. Formatting response")
-    conv
-  end
-
-  def route(%{ method: "GET", path: "/wildthings"} = conv) do
+  def route(%Conv{ method: "GET", path: "/wildthings"} = conv) do
     %{ conv | status: 200, resp_body: "Bears, Lions, Tigers"}
   end
 
-  def route(%{ method: "GET", path: "/bears"} = conv) do
+  def route(%Conv{ method: "GET", path: "/bears"} = conv) do
     %{ conv | status: 200, resp_body: "Teddy, Smokey, Paddington"}
   end
 
-  def route(%{ method: "GET", path: "/bears/new"} = conv) do
+  def route(%Conv{ method: "GET", path: "/bears/new"} = conv) do
     Path.expand("../../pages", __DIR__)
     |> Path.join("form.html")
     |> File.read
     |> handle_file(conv)
   end
 
-  def route(%{ method: "GET", path: "/bears/" <> id} = conv) do
+  def route(%Conv{ method: "GET", path: "/bears/" <> id} = conv) do
     %{ conv | status: 200, resp_body: "bear #{id}"}
   end
 
-  def route(%{ method: "DELETE", path: "/bears/" <> _id} = conv) do
+  def route(%Conv{ method: "DELETE", path: "/bears/" <> _id} = conv) do
     %{ conv | status: 403, resp_body: "Bears must never be deleted!"}
   end
 
-  def route(%{ method: "GET", path: "/about"} = conv) do
+  def route(%Conv{ method: "GET", path: "/about"} = conv) do
     @pages_path
     |> Path.join("about.html")
     |> File.read
     |> handle_file(conv)
   end
 
-  def route(%{ method: "GET", path: "/pages/" <> file} = conv) do
+  def route(%Conv{ method: "GET", path: "/pages/" <> file} = conv) do
     @pages_path
     |> Path.join(file <> ".html")
     |> File.read
     |> handle_file(conv)
   end
 
-  def route(%{ path: path} = conv) do
+  def route(%Conv{ path: path} = conv) do
     %{ conv | status: 404, resp_body: "No #{path} here!"}
   end
 
-  def handle_file({:ok, content}, conv) do
-    %{ conv | status: 200, resp_body: content}
-  end
-
-  def handle_file({:error, :enoent}, conv) do
-    %{ conv | status: 404, resp_body: "File not found!"}
-  end
-
-  def handle_file({:error, reason}, conv) do
-    %{ conv | status: 500, resp_body: "File error: #{reason}"}
-  end
-
-  def emojify(%{status: 200} = conv) do
+  def emojify(%Conv{status: 200} = conv) do
     emojies = String.duplicate("🚀", 5)
     body = emojies <> "\n" <> conv.resp_body <> "\n" <> emojies
 
     %{ conv | resp_body: body }
   end
 
-  def emojify(conv), do: conv
+  def emojify(%Conv{} = conv), do: conv
 
-  def format_response_header(conv) do
+  def format_response_header(%Conv{} = conv) do
 
     header = """
     Date: #{DateTime.utc_now()}
@@ -151,26 +86,15 @@ defmodule Servy.Handler do
     """
     %{ conv | resp_header: header }
   end
-  def format_response_body(conv) do
+  def format_response_body(%Conv{} = conv) do
     """
     #{conv.resp_header}
-    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
+    HTTP/1.1 #{Conv.full_status(conv)}
     Content-Type: text/html
     Content-Length: #{byte_size(conv.resp_body)}
 
     #{conv.resp_body}
     """
-  end
-
-  defp status_reason(code) do
-    %{
-      200 => "OK",
-      201 => "Created",
-      401 => "Unauthorized",
-      403 => "Forbidden",
-      404 => "Not Found",
-      500 => "Internal Server Error"
-    }[code]
   end
 
 end
---
+++ webdev/Elixir/servy/lib/servy/plugins.ex
@@ -0,0 +1,45 @@
+defmodule Servy.Plugins do
+  alias Servy.Conv
+  require Logger
+
+  def rewrite_path(%Conv{path: "/wildlife"} = conv) do
+    %{ conv | method: "GET", path: "/wildthings"}
+  end
+
+  def rewrite_path(%Conv{path: path} = conv) do
+    regex = ~r{\/(?<thing>\w+)\?id=(?<id>\d+)}
+    captures = Regex.named_captures(regex, path)
+    rewrite_path_captures(conv, captures)
+  end
+
+  def rewrite_path(%Conv{} = conv), do: conv
+
+  def rewrite_path_captures(conv, %{"thing" => thing, "id" => id}) do
+    %{ conv| path: "/#{thing}/#{id}}" }
+  end
+
+  def rewrite_path_captures(%Conv{} = conv, nil), do: conv
+
+  def track(%Conv{status: 404, path: path} = conv) do
+    Logger.warning(
+      "Warning: ⚠️⚠️⚠️ #{path} is not recognized as a valid endpoint⚠️⚠️⚠️",
+      [
+        date: DateTime.utc_now(),
+        request_id: to_string(:erlang.ref_to_list(:erlang.make_ref()))
+        ])
+    conv
+  end
+
+  def track(%Conv{} = conv), do: conv
+
+  def log_start(%Conv{} = conv) do
+    IO.inspect conv
+    Logger.info("#{conv.path} received. \nchecking for bad input🔍🔍🔍")
+    conv
+  end
+
+  def log_progress(%Conv{} = conv) do
+    Logger.info("Pre-checks succes✅. Formatting response")
+    conv
+  end
+end
