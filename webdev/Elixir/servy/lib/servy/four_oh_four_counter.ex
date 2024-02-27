564b6b00ae0e640a3c7ca38bd7982e6a3d3ac297
Wouter Dijks <wouterdijks@hotmail.com>
2024-02-27 14:19:40 +0100

    finished vid 25

--- webdev/Elixir/servy/lib/servy/four_oh_four_counter.ex
+++ webdev/Elixir/servy/lib/servy/four_oh_four_counter.ex
@@ -1,51 +1,67 @@
 defmodule Servy.FourOhFourCounter do
+  alias Servy.GenericServer
+
   @process_name :not_found_counter
 
   #Client interface
   def start(initial_state \\ %{}) do
     IO.puts "Starting the 404 counter..."
-    pid = spawn(__MODULE__, :listen_loop, [initial_state])
-    Process.register(pid, @process_name)
-    pid
+    GenericServer.start(__MODULE__, initial_state, @process_name)
+  end
+
+  def clear() do
+    GenericServer.cast(@process_name, :clear)
   end
 
   def bump_count(path) do
-    send(@process_name, {self(), :bump_count, path})
-
-    receive do {:response, count } -> count end
+    GenericServer.call(@process_name, {:bump_count, path})
   end
 
   def get_counts() do
-    send(@process_name, {self(), :get_counts})
-
-    receive do {:response, counts } -> counts end
+    GenericServer.call(@process_name, :get_counts)
   end
 
   def get_count(path) do
-    send(@process_name, {self(), :get_count, path})
-
-    receive do {:response, count } -> count end
+    GenericServer.call(@process_name, {:get_count, path})
   end
 
-  def listen_loop(state) do
-    receive do
-      {sender, :bump_count, path} ->
-        new_state = Map.update(state, path, 1, &(&1 + 1))
-        send(sender, {:response, Map.get(new_state, path)})
-        listen_loop(new_state)
+  # def listen_loop(state) do
+  #   receive do
+  #     {sender, :bump_count, path} ->
+  #       new_state = Map.update(state, path, 1, &(&1 + 1))
+  #       send(sender, {:response, Map.get(new_state, path)})
+  #       listen_loop(new_state)
 
-      {sender, :get_counts } ->
-        send(sender, {:response, state})
-        listen_loop(state)
+  #     {sender, :get_counts } ->
+  #       send(sender, {:response, state})
+  #       listen_loop(state)
 
-      {sender, :get_count, path } ->
-        count = Map.get(state, path, 0)
-        send sender, {:response, count}
-        listen_loop(state)
+  #     {sender, :get_count, path } ->
+  #       count = Map.get(state, path, 0)
+  #       send sender, {:response, count}
+  #       listen_loop(state)
 
-      unexpected ->
-        IO.puts("Unsuspected message #{inspect unexpected}")
-        listen_loop(state)
-    end
+  #     unexpected ->
+  #       IO.puts("Unsuspected message #{inspect unexpected}")
+  #       listen_loop(state)
+  #   end
+  # end
+
+  def handle_call({:bump_count, path}, state) do
+    new_state = Map.update(state, path, 1, &(&1 + 1))
+    {:ok, new_state}
+  end
+
+  def handle_call(:get_counts, state) do
+    {state, state}
+  end
+
+  def handle_call({:get_count, path}, state) do
+    count = Map.get(state, path, 0)
+    {count, state}
+  end
+
+  def handle_cast(:clear, _state) do
+    %{}
   end
 end
--- webdev/Elixir/servy/lib/servy/pledge_server.ex
+++ webdev/Elixir/servy/lib/servy/pledge_server.ex
@@ -1,54 +1,51 @@
 defmodule Servy.PledgeServer do
+
+  alias Servy.GenericServer
+
   @process_name :pledge_server
 
   #Client interface
-  def start(initial_state \\ []) do
+  def start() do
     IO.puts "Starting the Pledge Server..."
-    pid = spawn(__MODULE__, :listen_loop, [initial_state])
-    Process.register(pid, @process_name)
-    pid
+    GenericServer.start(__MODULE__, [], @process_name)
   end
 
   def create_pledge(name, amount) do
-    send(@process_name, {self(), :create_pledge, name, amount})
-
-    receive do {:response, status } -> status end
+    GenericServer.call(@process_name, {:create_pledge, name, amount})
   end
 
   def recent_pledges() do
-    send @process_name, {self(), :recent_pledges}
-
-    receive do {:response, pledges } -> pledges end
+    GenericServer.call(@process_name, :recent_pledges)
   end
 
   def total_pledged() do
-    send @process_name, {self(), :total_pledged}
-
-    receive do {:response, total } -> total end
+    GenericServer.call(@process_name, :total_pledged)
   end
 
-  # Server
-  def listen_loop(state) do
-    receive do
-      {sender, :create_pledge, name, amount} ->
-        {:ok, id } = send_pledge_to_service(name, amount)
-        most_recent_pledges = Enum.take(state, 2)
-        new_state = [ {name, amount} | most_recent_pledges ]
-        send(sender, {:response, id })
-        listen_loop(new_state)
+  def clear() do
+    GenericServer.cast(@process_name, :clear)
+  end
 
-      {sender, :recent_pledges } ->
-        send(sender, {:response, state})
-        listen_loop(state)
+  # Server callbacks
 
-      {sender, :total_pledged } ->
-        total = Enum.map(state, &elem(&1, 1)) |> Enum.sum()
-        send(sender, {:response, total})
-        listen_loop(state)
-      unexpected ->
-        IO.puts("Unsuspected message #{inspect unexpected}")
-        listen_loop(state)
-    end
+  def handle_cast(:clear, _state) do
+    []
+  end
+
+  def handle_call(:total_pledged, state) do
+    total = Enum.map(state, &elem(&1, 1)) |> Enum.sum()
+    {total, state}
+  end
+
+  def handle_call(:recent_pledges, state) do
+    {state, state}
+  end
+
+  def handle_call({:create_pledge, name, amount}, state) do
+    {:ok, id } = send_pledge_to_service(name, amount)
+    most_recent_pledges = Enum.take(state, 2)
+    new_state = [ {name, amount} | most_recent_pledges ]
+    {id, new_state}
   end
 
   defp send_pledge_to_service(_name, _amount) do
