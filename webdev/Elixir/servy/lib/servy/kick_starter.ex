a1f55c527057a4528bf3deb7acf30a439b8272a0
Wouter Dijks <wouterdijks@hotmail.com>
2024-02-29 10:24:00 +0100

    finish vid 28

--- webdev/Elixir/servy/lib/servy/kick_starter.ex
+++ webdev/Elixir/servy/lib/servy/kick_starter.ex
@@ -1,21 +1,37 @@
 defmodule Servy.KickStarter do
   use GenServer
 
+  # Client Interface
   def start do
     IO.puts("Starting the kickstarter")
     GenServer.start(__MODULE__, :ok, name: __MODULE__)
   end
 
+  def get_server, do: GenServer.call(__MODULE__, :get_server)
+
   def init(:ok) do
     Process.flag(:trap_exit, true)
-    IO.puts("Starting HTTP server")
-    server_pid = spawn(Servy.HttpServer, :start, [4000])
-    Process.link(server_pid)
-    Process.register(server_pid, :http_server)
+    server_pid = start_server()
     {:ok, server_pid}
   end
 
   def handle_info({:EXIT, _pid, reason}, _state) do
     IO.puts("HttpServer exited (#{inspect(reason)})")
+    server_pid = start_server()
+    {:ok, server_pid}
+    {:noreply, server_pid}
+  end
+
+  def handle_call(:get_server, _from, state) do
+    {:reply, state, state}
+  end
+
+  defp start_server do
+    IO.puts("Starting HTTP server")
+    server_pid = spawn_link(Servy.HttpServer, :start, [4000])
+    # spawn_link is more convenient than separate link call
+    # Process.link(server_pid)
+    Process.register(server_pid, :http_server)
+    server_pid
   end
 end
